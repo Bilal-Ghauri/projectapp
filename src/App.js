dbbf392dcae1bccade34aa63ed64492bba86dd5e
Bilal443 <63726992+Bilal443@users.noreply.github.com>
2020-06-05 17:35:35 +0500

    again update app.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
-          Updated by BILAL GHAURI
+          Agian Updated by BILAL GHAURI
         </p>
         <a
           className="App-link"
